dca4ccf94c45066f094d8ebf85d2ab06ee098f13
Ferid Mirzeyev <feridmrzyv2005@gmail.com>
2026-07-08 16:59:33 +0400

    fix github error

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,3 @@
-// Suppress missing type declarations if the package's types aren't installed
-// @ts-ignore: Could not find module 'react-router-dom' or its type declarations
 import { HashRouter } from "react-router-dom";
 import { createRoot } from "react-dom/client";
 import App from "./App";
