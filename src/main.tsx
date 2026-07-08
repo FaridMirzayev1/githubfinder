756032a956f8b4ee299c118ca32174ace0fe0f0e
Ferid Mirzeyev <feridmrzyv2005@gmail.com>
2026-07-08 16:50:13 +0400

    update

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
-import { StrictMode } from "react";
+// Suppress missing type declarations if the package's types aren't installed
+// @ts-ignore: Could not find module 'react-router-dom' or its type declarations
+import { HashRouter } from "react-router-dom";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
+  <HashRouter>
     <App />
-  </StrictMode>,
+  </HashRouter>,
 );
